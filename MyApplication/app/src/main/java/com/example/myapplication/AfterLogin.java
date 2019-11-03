9cd6af25a65dd1b13a09223425375e4b2722821c
uhj1250 <37183595+uhj1250@users.noreply.github.com>
2019-11-04 04:14:50 +0900

    구글 로그인

--- MyApplication/app/src/main/java/com/example/myapplication/AfterLogin.java
+++ MyApplication/app/src/main/java/com/example/myapplication/AfterLogin.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.kakao.network.ErrorResult;
-import com.kakao.usermgmt.UserManagement;
-import com.kakao.usermgmt.callback.LogoutResponseCallback;
-import com.kakao.usermgmt.callback.MeV2ResponseCallback;
-import com.kakao.usermgmt.response.MeV2Response;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,45 +21,23 @@ import java.util.List;
 public class AfterLogin extends Activity {
     private Button btn_logout;
     private Button btn_openmap;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.after_login);
-        List<String> keys = new ArrayList<>();
-        keys.add("properties.nickname");
-        keys.add("properties.profile_image");
-        //keys.add("kakao_account.email");
-
-
-        UserManagement.getInstance().me(keys,new MeV2ResponseCallback(){
-            TextView id_info = (TextView)findViewById(R.id.id_info);
-           @Override
-            public void onFailure(ErrorResult errorResult){
-               id_info.setText("errorResult");
-           }
-
-           @Override
-           public void onSessionClosed(ErrorResult errorResult){
-               id_info.setText("errorResult");
-           }
-
-           public void onSuccess(MeV2Response response){
-               id_info.setText("id: " + response.getId());
-           }
-        });
-
+        Intent intentgetter = getIntent();
+        TextView info = (TextView)findViewById(R.id.id_info);
+        String userID = intentgetter.getStringExtra("id");
+        String userIDToken = intentgetter.getStringExtra("id_token");
+        info.setText(userID + ", token: " + userIDToken);
 
         btn_logout = (Button) findViewById(R.id.btn_logout);
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
-                    @Override
-                    public void onCompleteLogout() {
-                        Intent intent = new Intent(AfterLogin.this, LoginActivity.class);
-                        startActivity(intent);
-                    }
-                });
+                Intent intent = new Intent(AfterLogin.this, LoginActivity.class);
+                intent.putExtra("logout",true);
+                startActivity(intent);
+                finish();
             }
         });
 
@@ -65,15 +45,12 @@ public class AfterLogin extends Activity {
         btn_openmap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
-                    @Override
-                    public void onCompleteLogout() {
-                        Intent intent = new Intent(AfterLogin.this, MapViewActivity.class);
-                        startActivity(intent);
-                    }
-                });
+                Intent intent = new Intent(AfterLogin.this, MapViewActivity.class);
+                startActivity(intent);
             }
         });
     }
+
+
 }
 
