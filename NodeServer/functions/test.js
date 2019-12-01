93d5b92c7085bd671b5acece9b2a18b386b42515
JunHyeon Yoon <junhyun328@gmail.com>
2019-12-02 05:04:55 +0900

    add calculate_rate function in test.js

--- NodeServer/functions/test.js
+++ NodeServer/functions/test.js
@@ -9,21 +9,29 @@ var vector = function(posX,posY){
     this.y = posY;
 }
 
-var list = function(){
-    this.content = [];
-    this.list_size = 0;
-}
-
-list.prototype.append = function(element){
-    this.content[this.list_size] = element;
-    this.list_size++;
-}
-
 var search_index = function(graph, id){
     for(i in graph.node){
         if(graph.node[i] == id)
             return i;
     }
+}
+
+var sum = function(array){
+    total = 0;
+    for(i in array){
+        sum = sum + array[i];
+    }
+    return total;
+}
+
+var calculate_rate(angle_list){
+    var rate_list = [];
+    total = sum(angle_list) + angle_list.length;
+    for(i in angle_list){
+        rate_list.push( (angle_list[i]+1) / total );
+    }
+    
+    return rate_list;
 }
 
 var vector_size = function(vec){
@@ -37,16 +45,17 @@ var calculate_angle = function(vec1, vec2){
 }
 
 var choose_next_node = function(graph, center_index, before_index){
-    var angle_list = new list();
+    var angle_list = [];
     var stn_vec = new vector(graph.node[before_index].latitude - graph.node[center_index].latitude, graph.node[before_index].longitude - graph.node[center_index].longitude);
     for(i in graph.node[center_index].link){
         var adjacent_index = search_index(graph, graph.node[center_index].link[i]);
         if(adjacent_index == before_index)
-            angle_list.append(0);
+            angle_list.push(0);
         else{
             var ind_vec = new vector(graph.node[adjacent_index].latitude - graph.node[center_index].latitude, graph.node[adjacent_index].longitude - graph.node[center_index].longitude);
-            angle_list.append(calculate_angle(stn_vec,ind_vec));
+            angle_list.push(calculate_angle(stn_vec,ind_vec));
         }
     }
+    var smooth_rate_list = calculate_rate(angle_list);
     
 }
