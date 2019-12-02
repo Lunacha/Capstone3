b282bf8a8b564335c9a361069113d2bf3d2c36cc
UniJang <53331484+UniJang@users.noreply.github.com>
2019-12-02 21:38:10 +0900

    수정 필요한부분 주석되어있음

--- MyApplication/app/src/main/java/com/example/myapplication/AfterLogin.java
+++ MyApplication/app/src/main/java/com/example/myapplication/AfterLogin.java
@@ -1,11 +1,16 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
 
 
 import androidx.annotation.NonNull;
@@ -19,39 +24,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AfterLogin extends Activity {
-    private Button btn_logout;
-    private Button btn_openmap;
+    private Button createroom;
+    private Button joinroom;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.after_login);
         Intent intentgetter = getIntent();
-        TextView info = (TextView)findViewById(R.id.id_info);
+        TextView info = (TextView) findViewById(R.id.id_info);
         String userID = intentgetter.getStringExtra("id");
-        String userIDToken = intentgetter.getStringExtra("id_token");
-        info.setText(userID + ", token: " + userIDToken);
+        info.setText(userID);
 
-        btn_logout = findViewById(R.id.btn_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AfterLogin.this, LoginActivity.class);
-                intent.putExtra("logout",true);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        btn_openmap = findViewById(R.id.btn_open_map);
-        btn_openmap.setOnClickListener(new View.OnClickListener() {
+        final ImageButton createroom = (ImageButton) findViewById(R.id.createroom); //Map_open 으로 연결되게만 우선 해둠
+        createroom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(AfterLogin.this, MapViewActivity.class);
-                intent.putExtra("uid",userID);
+                intent.putExtra("uid", userID);
                 startActivity(intent);
+            }
+        });
+
+        final ImageButton joinroom = (ImageButton) findViewById(R.id.joinroom);
+        joinroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show();
             }
         });
     }
 
+    void show(){
+        final EditText edittext = new EditText(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("방 참가하기");
+        builder.setMessage("참가하려는 방의 코드를 입력해주세요.");
+        builder.setView(edittext);
+        builder.setPositiveButton("입력", new DialogInterface.OnClickListener() {
+            String input_key;
+            public void onClick(DialogInterface dialog, int which) {
+                input_key = edittext.getText().toString();
+                //여기서 키 받고 맞으면 그 뒤 작업 추가
+                //틀리면 안내메시지 출력
+            }
+        });
+        builder.setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.show();
+    }
 
 }
-
