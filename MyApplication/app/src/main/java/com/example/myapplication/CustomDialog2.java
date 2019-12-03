06ac9a0f01a18e105edd98b5f8d2fcdd348c0974
UniJang <53331484+UniJang@users.noreply.github.com>
2019-12-03 23:01:56 +0900

    Update CustomDialog2.java

--- MyApplication/app/src/main/java/com/example/myapplication/CustomDialog2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/CustomDialog2.java
@@ -47,7 +47,7 @@ public class CustomDialog2 {
                 targetheight = Integer.parseInt(edittext.getText().toString());
                 TimePicker time = (TimePicker)dlg.findViewById(R.id.losttime);
 
-                speed = (float)targetheight - 0.16;
+                speed = (float)targetheight/100.0 - 0.16;
                 hour = time.getHour();
                 min = time.getMinute();
                 pasttime = hour * 3600000 + min * 60000;
